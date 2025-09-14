c67f5d5326692195c515a86aebcef70a788570f6
PotterIle <elindeda1@gmail.com>
2025-09-14 09:58:55 +0300

    backend spring app

---
+++ backend/agriconnectke/src/main/java/com/ecommerce/agriconnectke/AgriconnectkeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.agriconnectke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriconnectkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriconnectkeApplication.class, args);
+	}
+
+}
---
+++ backend/agriconnectke/src/test/java/com/ecommerce/agriconnectke/AgriconnectkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.agriconnectke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriconnectkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

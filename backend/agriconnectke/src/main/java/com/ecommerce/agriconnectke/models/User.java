a291426bd82bb7aa81a315082aa5533cb928d73a
tiffm <tiff.maina01@gmail.com>
2025-11-10 16:22:16 +0300

    modified user parameters.

--- backend/agriconnectke/src/main/java/com/ecommerce/agriconnectke/models/User.java
+++ backend/agriconnectke/src/main/java/com/ecommerce/agriconnectke/models/User.java
@@ -15,7 +15,7 @@ import jakarta.persistence.Table;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
+    @Column(name =  "user_id")
     private Long userId;
 
     @Column(name="full_name", nullable=false)

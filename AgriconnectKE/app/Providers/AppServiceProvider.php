9fb38bcf60de14b6e83432058cf26e1f5e2f5e3d
PotterIle <elindeda1@gmail.com>
2025-11-10 17:07:43 +0300

    service providers

--- AgriconnectKE/app/Providers/AppServiceProvider.php
+++ AgriconnectKE/app/Providers/AppServiceProvider.php
@@ -1,24 +1,25 @@
 <?php
+// app/Providers/AppServiceProvider.php
 
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use App\Http\Middleware\CheckRole;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
-        //
+        $this->app->singleton('role', function ($app) {
+            return new CheckRole();
+        });
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Blade::if('role', function ($role) {
+            return auth()->check() && auth()->user()->role === $role;
+        });
     }
 }

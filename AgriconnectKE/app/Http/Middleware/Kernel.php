badc2321cbe8f648a4489a7c09a2a797bd41d3a6
Eniola Faithfulness Fabunmi <eniola.fabunmi@strathmore.edu>
2025-11-10 23:34:07 +0300

    additional auth file update

---
+++ AgriconnectKE/app/Http/Middleware/Kernel.php
@@ -0,0 +1,16 @@
+<?php
+// app/Http/Kernel.php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    // ... existing code ...
+
+    protected $routeMiddleware = [
+        // ... other middleware ...
+        'role' => \App\Http\Middleware\CheckRole::class,
+    ];
+}

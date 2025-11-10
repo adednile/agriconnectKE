e21f97bc3da020fb5cef3f0a955c67d22d09abec
kadaji777 <162417406+kadaji777@users.noreply.github.com>
2025-11-10 16:18:01 +0300

    Delete frontend/agriconnect-ke directory

--- frontend/agriconnect-ke/src/services/orderService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/orders/";
-
-export const getOrders = () => axios.get(API_URL);
-export const createOrder = (order) => axios.post(API_URL, order);

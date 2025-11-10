196696f720db3fe8d79b24aa2c8c48fd02c62f27
Eli <elindeda1@gmail.com>
2025-11-10 16:17:57 +0300

    Delete backend directory

--- backend/agriconnectke/src/main/java/com/ecommerce/agriconnectke/repositories/DriverLocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.agriconnectke.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.agriconnectke.models.DriverLocation;
-public interface DriverLocationRepository extends JpaRepository<DriverLocation, Long> {
-        DriverLocation findTopByDriverIdOrderByTimestampDesc(Long driverId);
-
-}
--- backend/agriconnectke/src/main/java/com/ecommerce/agriconnectke/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.agriconnectke.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.agriconnectke.models.Order;
-public interface OrderRepository extends JpaRepository<Order, Long>  {
-
-}

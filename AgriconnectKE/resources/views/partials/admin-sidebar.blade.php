5fe9df19fe4942aac1c4c9169d4258d275cc93e7
tiffm <tiff.maina01@gmail.com>
2025-11-11 08:25:47 +0300

    add: admin sidebar navigation with dashboard link

---
+++ AgriconnectKE/resources/views/partials/admin-sidebar.blade.php
@@ -0,0 +1,8 @@
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link active" href="{{ route('admin.dashboard') }}">
+            Dashboard
+        </a>
+    </li>
+
+</ul>

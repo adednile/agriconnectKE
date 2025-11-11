e79ee0dbb73184cd60fb85ab0d8f12633867a511
tiffm <tiff.maina01@gmail.com>
2025-11-11 08:37:47 +0300

    add: driver sidebar blade view for navigation structure

---
+++ AgriconnectKE/resources/views/partials/driver-sidebar.blade.php
@@ -0,0 +1,8 @@
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link active" href="{{ route('driver.dashboard') }}">
+            Dashboard
+        </a>
+    </li>
+
+</ul>
